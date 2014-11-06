823db3eac8ed71566ff7b0a69f59f1fd6d82517c
yaskhan <yaskhan@yandex.ru>
2014-11-06 12:37:06 +0600

    readme and test file

---
+++ readme.md
@@ -0,0 +1,66 @@
+py2php
+------
+транслятор с языка программирования Python в PHP
+
+
+TODO
+-----
+- Import и пасинг загруженного кода
+- Import from
+- Import alias
+- TryExcept
+- TryFinally
+- function vararg
+- function kwarg
+- Nested функций и классы
+- Добавлять в начало функций static и abstract если есть в декораторах @abstract, @static
+- писать при вызове статических и константных аттрибутов «::»(Paamayim Nekudotayim) вместо «->», а это обычно аттрибуты написанные прописными буквами (константы), или методы с декоратором @staticmethod
+- генерировать интерфейс класс если у класса есть декоратор interface
+- доработать lambda
+- доработать for
+- yeld
+- with
+- написать консольную утилиту для конвертаций XML или JSON, созданный документогенераторами PHP, в Python (генератор оберток)
+
+Пример
+------
+Код:
+```python
+class Asd(A):
+    f = []
+    g = ""
+    h = 0
+    def __init__(self):
+        self.g = True
+        
+    def _foo(self):
+        self.publiddsds(a,d)
+    
+    def publiddsds(self):
+        pass
+```
+переведется в:
+
+```php
+class Asd extends A {
+    protected $f = array();
+    protected $g = '';
+    protected $h = 0;
+
+    function __construct () {
+        $this->g = true;
+    }
+
+    protected function _foo() {
+        $this->publiddsds($a, $d);
+    }
+
+    public function publiddsds() {
+        ;
+    }
+}
+```
+
+Лицензия
+--------
+MIT
---
+++ test_file.py
@@ -0,0 +1,118 @@
+from wrapper.spl.containers.heap import SplHeap
+from wrapper.spl.containers.stack import SplStack as Stack
+from wrapper.spl.exceptions import * 
+from wrapper.spl.exceptions import BadFunctionCallException as bad, RuntimeException
+
+raise Exception("Errrrrrrooooooooooor!!")
+
+
+
+
+sta = Stack()
+sta.push("11")
+sta.push("11")
+sta.push("11")
+
+sta.offsetSet(0, "C")
+
+sta.rewind()
+
+while sta.valid():
+    print(sta.current(), "<br>")
+    sta.next()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#import sys, os.abort
+aa = {"a":1, "b":2}
+for a,d in aa.items():
+    print(a, " : ", d)
+    
+
+class Asd(A):
+    f = []
+    g = ""
+    h = 0
+    def __init__(self):
+        self.g = True
+        
+    
+    def _foo(self):
+        self.publiddsds(a,d)
+    
+    def publiddsds(self):
+        pass
+
+
+d = str(Asd(_GET["abra"]))
+var = None
+
+a = [1,2,var]
+
+aa = {
+     "a":1, 
+     "b":2, 
+     "c":
+       {"d":3}
+    }
+
+if "a" in aa:
+  print("1")
+elif isinstance(d, Asd):
+  for v , a in enumerate(d):
+      if var == 4:
+          print("3")
+else:
+  print(print("2"))
+
+del var, aa
+
+def foo(a,b):
+  print("1")
+  
+
+def echo(value=None):
+  print("Execution starts when 'next()' is called for the first time.")
+  try:
+      while True:
+          try:
+              value = (yield value)
+          except Exception as e:
+              value = e
+  finally:
+      print("Don't forget to clean up when 'close()' is called.")
+
+
+d = {"f":1}
+#self.globall()
+#"str".join([1,2,3])
+def make_incrementor(n):
+   return lambda x: x + n
+
+
+@enclose("()")
+def join(a):
+   return str_join(str)
+
+
+def enclose(enclosure, func = None):
+  def decorator(func):
+      def newfunc(self, node):
+          self.write(enclosure[0])
+          func(self, node)
+          self.write(enclosure[-1])
+      return newfunc
+  return decorator
